ce3205c1b13eb3378a722dcb9201fea73a4963e4
Anoop Elias <anoopelias@gmail.com>
2020-03-16 11:12:46 +0530

    <Anoop>|<Tapas> Added hello URL support
    Signed-off-by: Anoop Elias <anoopelias@gmail.com>

--- src/main/java/com/w2w/What2Watch/What2WatchApplication.java
+++ src/main/java/com/w2w/What2Watch/What2WatchApplication.java
@@ -2,12 +2,20 @@ package com.w2w.What2Watch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class What2WatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(What2WatchApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
