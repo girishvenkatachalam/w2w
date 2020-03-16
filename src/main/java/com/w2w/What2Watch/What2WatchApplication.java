48c5deec7cc276409f4a617af9109a1ab40b9dc4
GirishV <girish.v1@thoughtworks.com>
2020-03-16 09:59:55 +0530

    []|Initial Project Setup with Spring Boot|[GirishV]

---
+++ src/main/java/com/w2w/What2Watch/What2WatchApplication.java
@@ -0,0 +1,13 @@
+package com.w2w.What2Watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class What2WatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(What2WatchApplication.class, args);
+	}
+
+}
