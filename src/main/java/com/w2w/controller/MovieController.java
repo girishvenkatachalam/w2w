a7f94a600eee11e757f408c2801853e740d4d768
kamaleshkohli <kamaleshkohli@gmail.com>
2020-03-17 16:52:56 +0530

    <Soumya>|<Kamalesh> Added dummy code to test api call for Movie Dashboard

--- src/main/java/com/w2w/controller/MovieController.java
+++ src/main/java/com/w2w/controller/MovieController.java
@@ -1,7 +1,29 @@
 package com.w2w.controller;
 
+import com.w2w.model.Movie;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequestMapping("/movie-details")
 public class MovieController {
+
+    @RequestMapping(value="/home", method = RequestMethod.GET)
+    public String getMovieDetails(ModelMap modelMap)
+    {
+        List<Movie> movies = new ArrayList<>();
+        Movie movie = new Movie();
+        movie.id = 0;
+        movie.homepage = "abc";
+        movie.title = "Avatar";
+        movies.add(movie);
+        modelMap.addAttribute("name", "Trending");
+        modelMap.addAttribute("list", movies);
+        return "home";
+    }
 }
