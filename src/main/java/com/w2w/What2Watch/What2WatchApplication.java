f0c76d3dd27d442a5ac142ca1456af598121d7be
Anoop Elias <anoopelias@gmail.com>
2020-03-17 15:41:10 +0530

    <Anoop> Fixed compilation error. Added missing java.util.List
    Signed-off-by: Anoop Elias <anoopelias@gmail.com>

--- src/main/java/com/w2w/What2Watch/What2WatchApplication.java
+++ src/main/java/com/w2w/What2Watch/What2WatchApplication.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class What2WatchApplication {
