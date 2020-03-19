2ea2f1b22678f976af01ca036cae0c825f9c10d6
Anoop Elias <anoopelias@gmail.com>
2020-03-19 06:40:48 +0530

    <Anoop> Experimental: Use react based google login

--- src/main/frontend/src/containers/LoginPage/index.js
+++ src/main/frontend/src/containers/LoginPage/index.js
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import texts from "../../texts.json";
 import "./index.scss";
 import { GoogleLogin } from 'react-google-login';
+const responseGoogle = (response) => {
+  console.log(response);
+}
 
 const LoginPage = () => (
   <div className="login-container">
@@ -40,5 +43,13 @@ const LoginPage = () => (
     </div>
   </div>
 );
+
+// const LoginPage = () => <div className="login-container"><GoogleLogin
+//     clientId="739919577396-k7710v3cjvccnt0t8vu70c81kk63j7i1.apps.googleusercontent.com"
+//     buttonText="Login"
+//     onSuccess={responseGoogle}
+//     onFailure={responseGoogle}
+//     cookiePolicy={'single_host_origin'}
+// /></div>;
 
 export default LoginPage;
