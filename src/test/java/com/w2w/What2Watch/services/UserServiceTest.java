1821eb2ab7820223c3e269ec6830fe2775665bca
jyoti2106j <jyoti2106j@gamil.com>
2020-03-20 12:24:10 +0530

    <Jyoti>|<Shebin> Added Servicelayer test case for adding genreto user

--- src/test/java/com/w2w/What2Watch/services/UserServiceTest.java
+++ src/test/java/com/w2w/What2Watch/services/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.w2w.What2Watch.services;
 
 import com.w2w.What2Watch.exceptions.UserNotFoundException;
+import com.w2w.What2Watch.models.Genre;
 import com.w2w.What2Watch.models.User;
 import com.w2w.What2Watch.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -78,4 +81,67 @@ public class UserServiceTest {
         assertEquals("User with email \"asd@qwqw\" not found", exception.getMessage());
     }
 
+    @Test
+    void shouldAddGenreToUserSuccessfullyByGivenEmail() throws UserNotFoundException {
+        User user=new User();
+        user.userId="123";
+        user.name="Tanvi";
+        user.email="tanvi@movie.com";
+
+        when(userRepository.findByEmail("tanvi@movie.com")).thenReturn(user);
+        List<Genre> genres=user.getGenres();
+        Genre genre = new Genre();
+        genre.id=12;
+        genre.name="Adventure";
+
+        when(userRepository.save(user)).thenReturn(user);
+
+        User actualUser = userService.addGenreToUserByEmail("tanvi@movie.com",genre);
+        Genre actualGenre= actualUser.getGenres().get(0);
+
+        assertEquals("Tanvi", actualUser.getName());
+        assertEquals("tanvi@movie.com", actualUser.getEmail());
+        assertEquals(1, user.getGenres().size());
+        assertEquals(12, actualGenre.id);
+        assertEquals("Adventure", actualGenre.name);
+
+    }
+
+    @Test
+    void shouldNotAddDuplicateGenreToUserSuccessfullyByGivenEmail() throws UserNotFoundException {
+        User user=new User();
+        user.userId="123";
+        user.name="Tanvi";
+        user.email="tanvi@movie.com";
+
+        when(userRepository.findByEmail("tanvi@movie.com")).thenReturn(user);
+        List<Genre> genres=user.getGenres();
+        Genre genre = new Genre();
+        genre.id=12;
+        genre.name="Adventure";
+
+        when(userRepository.save(user)).thenReturn(user);
+        User actualUser = userService.addGenreToUserByEmail("tanvi@movie.com",genre);
+        Genre actualGenre= actualUser.getGenres().get(0);
+
+        when(userRepository.findByEmail("tanvi@movie.com")).thenReturn(actualUser);
+
+        List<Genre> genresNext=actualUser.getGenres();
+        Genre genreNext = new Genre();
+        genreNext.id=12;
+        genreNext.name="Adventure";
+
+        when(userRepository.save(actualUser)).thenReturn(actualUser);
+
+        User actualUserNext = userService.addGenreToUserByEmail("tanvi@movie.com",genreNext);
+        Genre actualGenreNext= actualUserNext.getGenres().get(0);
+
+        assertEquals("Tanvi", actualUser.getName());
+        assertEquals("tanvi@movie.com", actualUser.getEmail());
+        assertEquals(1, user.getGenres().size());
+
+        assertEquals(12, actualGenre.id);
+        assertEquals("Adventure", actualGenre.name);
+
+    }
 }
